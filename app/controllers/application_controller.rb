4c602ed55d766af9f15755a98bdafc1279a1c058
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-11-29 18:31:30 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+       erb :index 
+    end
 
+    post '/teams' do
+        @team=Team.new(params[:team])
+
+        params[:team][:heros].each do |info|
+         Hero.new(info)
+        end
+        @heros=Hero.all
+ 
+        erb :team
+    end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,8 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require_all 'app'
+require_relative '../app/controllers/application_controller'
+require_relative '../app/models/team'
+require_relative '../app/models/heros'
+
+require 'capybara/dsl'
